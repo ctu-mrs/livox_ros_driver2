56d34e2bbf1114ce97f6474c589223262fcdaede
Pavel Petracek <petrapa6@fel.cvut.cz>
2024-07-02 16:42:21 +0200

    update uninstall.sh

--- installation/uninstall.sh
+++ installation/uninstall.sh
@@ -4,5 +4,5 @@ SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
 sudo rm -rf /usr/local/lib/liblivox_lidar_sdk_*
 sudo rm -rf /usr/local/include/livox_lidar_*
 
-rm package.xml
-rm CMakeLists.xml
+rm ${SCRIPT_PATH}/../package.xml
+rm ${SCRIPT_PATH}/../CMakeLists.txt
