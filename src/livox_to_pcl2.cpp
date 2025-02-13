74a837087590bd821cbed1801d631170ff7860f5
veskrdan <veskrna@fly4future.com>
2025-02-13 11:15:34 +0100

    Add nodelet converting livox pcl to ROS pcl2

---
+++ src/livox_to_pcl2.cpp
@@ -0,0 +1,103 @@
+#include <livox_to_pcl2.h>
+
+namespace livox_ros
+{
+
+/* onInit() //{ */
+void LivoxPCLtoPCL2::onInit() {
+
+  ros::NodeHandle nh = nodelet::Nodelet::getMTPrivateNodeHandle();
+  ros::Time::waitForValid();
+
+  mrs_lib::SubscribeHandlerOptions shopts(nh);
+  shopts.node_name          = "LivoxPCLtoPCL2";
+  shopts.no_message_timeout = ros::Duration(5.0);
+
+  _sub_lidar_livox_pcl = mrs_lib::SubscribeHandler<livox_ros_driver2::CustomMsg>(shopts, "lidar_in", &LivoxPCLtoPCL2::lidarLivoxCallback, this);
+
+  _pub_lidar_pcl2 = nh.advertise<sensor_msgs::PointCloud2>("lidar_out", 1);
+
+  NODELET_INFO_ONCE("[LivoxPCLtoPCL2] Nodelet initialized");
+
+  is_initialized = true;
+}
+//}
+
+/* lidarLivoxCallback() //{ */
+// Convert livox_ros_driver2::CustomMsg to sensor_msgs::PointCloud2 message type
+void LivoxPCLtoPCL2::lidarLivoxCallback(const livox_ros_driver2::CustomMsg::ConstPtr msg) {
+
+  if (!is_initialized) {
+    return;
+  }
+
+  const sensor_msgs::PointCloud2::Ptr cloud_ros = boost::make_shared<sensor_msgs::PointCloud2>();
+
+  convertLivoxPCLtoPCL2(cloud_ros, msg);
+
+  _pub_lidar_pcl2.publish(cloud_ros);
+}
+//}
+
+/*//{ ConvertLivoxPCLtoPCL2() */
+void convertLivoxPCLtoPCL2(const sensor_msgs::PointCloud2::Ptr &cloud, const livox_ros_driver2::CustomMsg::ConstPtr &livox_msg) {
+  cloud->header = livox_msg->header;
+  cloud->fields.resize(6);
+
+  cloud->fields[0].offset   = 0;
+  cloud->fields[0].name     = "x";
+  cloud->fields[0].count    = 1;
+  cloud->fields[0].datatype = sensor_msgs::PointField::FLOAT32;
+
+  cloud->fields[1].offset   = 4;
+  cloud->fields[1].name     = "y";
+  cloud->fields[1].count    = 1;
+  cloud->fields[1].datatype = sensor_msgs::PointField::FLOAT32;
+
+  cloud->fields[2].offset   = 8;
+  cloud->fields[2].name     = "z";
+  cloud->fields[2].count    = 1;
+  cloud->fields[2].datatype = sensor_msgs::PointField::FLOAT32;
+
+  cloud->fields[3].offset   = 12;
+  cloud->fields[3].name     = "intensity";
+  cloud->fields[3].count    = 1;
+  cloud->fields[3].datatype = sensor_msgs::PointField::FLOAT32;
+
+  cloud->fields[4].offset   = 16;
+  cloud->fields[4].name     = "tag";
+  cloud->fields[4].count    = 1;
+  cloud->fields[4].datatype = sensor_msgs::PointField::UINT8;
+
+  cloud->fields[5].offset   = 17;
+  cloud->fields[5].name     = "line";
+  cloud->fields[5].count    = 1;
+  cloud->fields[5].datatype = sensor_msgs::PointField::UINT8;
+
+  cloud->point_step         = 18;
+  cloud->row_step           = cloud->point_step * livox_msg->point_num;
+  cloud->data.resize(cloud->row_step);
+
+  uint8_t* livox_data_ptr = cloud->data.data();
+    for (const auto& point : livox_msg->points)
+    {
+        *(reinterpret_cast<float*>(livox_data_ptr + 0)) = point.x;
+        *(reinterpret_cast<float*>(livox_data_ptr + 4)) = point.y;
+        *(reinterpret_cast<float*>(livox_data_ptr + 8)) = point.z;
+        *(reinterpret_cast<float*>(livox_data_ptr + 12)) = static_cast<float>(point.reflectivity);
+        *(livox_data_ptr + 16) = point.tag;
+        *(livox_data_ptr + 17) = point.line;
+
+        livox_data_ptr += cloud->point_step;
+    }
+
+    cloud->width = livox_msg->point_num;
+    cloud->height = 1;
+    cloud->is_bigendian = false;
+    cloud->is_dense = true;
+}
+/*//}*/
+
+}  // namespace livox_ros
+
+PLUGINLIB_EXPORT_CLASS(livox_ros::LivoxPCLtoPCL2, nodelet::Nodelet);
