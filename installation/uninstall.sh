8f6cf644c6a3b02aa61b109da8ff76ecf3fc22e2
Pavel Petracek <petrapa6@fel.cvut.cz>
2025-03-24 15:07:51 +0100

    add sdk-lib caching to install.sh

--- installation/uninstall.sh
+++ installation/uninstall.sh
@@ -2,6 +2,9 @@
 
 SCRIPT_PATH="$( cd "$(dirname "$0")" || exit ; pwd -P )"
 
+echo "Removing Livox SDK libraries"
+rm -rf ${SCRIPT_PATH}/../3rdparty/install
+
 echo "Removing links to CMakeLists.txt and package.xml"
 rm ${SCRIPT_PATH}/../package.xml
 rm ${SCRIPT_PATH}/../CMakeLists.txt
